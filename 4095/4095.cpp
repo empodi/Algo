ff3aa92542e325bf3ce3e7f3953cfcfe522a12a3
empodi <kglein9513@gmail.com>
2022-03-17 12:33:42 +0900

    4095 최대 정사각형 - 골드 5 - dp 어려워요

---
+++ 4095/4095.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int height;
+int width;
+int dp[1001][1001];
+
+int getMin(int a, int b, int c) {
+    
+    a = a < b ? a : b;
+    
+    return a < c ? a : c;
+}
+
+int solve() {
+    
+    int answer = 0;
+    
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cin >> dp[i][j];
+            
+            if ((i == 0 || j == 0) && dp[i][j]) {
+                answer = 1;
+            }
+        }
+    }
+    
+    for (int i = 1; i < height; i++) {
+        for (int j = 1; j < width; j++) {
+            
+            if (dp[i][j] == 0) continue;
+            
+            dp[i][j] = getMin(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+            
+            answer = answer > dp[i][j] ? answer : dp[i][j];
+        }
+    }
+    
+    return answer;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    while (1) {
+        
+        cin >> height >> width;
+        
+        if (height == 0 && width == 0) {
+            break;
+        }
+        
+        cout << solve() << "\n";
+    }
+    
+    return 0;
+}
