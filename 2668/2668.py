124c60d82f4d8771081c5f68ac7a632fe5ff85e0
empodi <kglein9513@gmail.com>
2022-10-18 13:45:46 +0900

    숫자고르기 - 골드 5 - dfs 사이클 판별

---
+++ 2668/2668.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+N = int(input().strip())
+A = [0]
+for _ in range(N):
+    A.append(int(input().strip()))
+
+done = [False for _ in range(N+1)]
+searching = [False for _ in range(N+1)]
+ans, tmp, target = [], [], 0
+
+def dfs(cur):
+    global target, flag
+    if cur == target:
+        return True
+    if done[cur] or searching[cur]:
+        return False
+    tmp.append(cur)
+    searching[cur] = True
+    return False | dfs(A[cur])
+    
+for i in range(1,N+1):
+    if not done[i]:
+        tmp, target = [i], i
+        if dfs(A[i]):
+            ans.extend(tmp)
+            for e in tmp:
+                done[e] = True
+        searching = [False for _ in range(N+1)]
+                
+ans = sorted(list(set(ans)))
+print(len(ans))
+for e in ans:
+    print(e)
---
+++ 2668/README.md
@@ -0,0 +1,5 @@
+# 백준 2668 숫자고르기
+
+[BOJ 2668](https://www.acmicpc.net/problem/2668)
+
+### 골드 5
