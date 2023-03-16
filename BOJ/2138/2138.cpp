1bbaf160350c2814c84a305c0b97ad182c490878
empodi <kglein9513@gmail.com>
2023-03-16 11:50:37 +0900

    ❗전구와 스위치 - 골드 5 - 그리디

--- BOJ/2138/2138.cpp
+++ BOJ/2138/2138.cpp
@@ -1,63 +1,64 @@
-#include <cstdlib>
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
+#define LEN 100000
 
-void change(string& a, const int& pos, const int& N)
-{
-    for (int i = pos - 1; i <= pos + 1; i++)
-    {
-        if (i < 0 || i >= N) continue;
-        if (a[i] == '0') a[i] = '1';
-        else a[i] = '0';
-    }
-}
+int N;
+int A[LEN + 1] = {0,}, B[LEN + 1] = {0,}, C[LEN + 1] = {0,};
 
-int firstSolve(string& a, const string& target, const int& N)    // 첫 버튼 안 누른 경우
-{
-    int ret = 0;
-    for (int i = 1; i < N; i++)
-    {
-        if (a[i - 1] != target[i - 1]) 
-        {
-            change(a, i, N);
-            ret++;
-        }
-        if (a == target) return ret;
-    }
-    return -1;
-}
-
-int secondSolve(string& a, string& target, const int& N)    // 첫 버튼 누른 경우
-{
-    int ret = 1;
-    change(a, 0, N);
-    for (int i = 1; i < N; i++)
-    {
-        if (a[i - 1] != target[i - 1]) 
-        {
-            change(a, i, N);
-            ret++;
-        }
-        if (a == target) return ret;
-    }
-    return -1;
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    string a, b, target;
-    int N;
+void get_input() {
     cin >> N;
-    cin >> a;
-    cin >> target;
-    b = a;
-    int x = firstSolve(a, target, N);
-    int y = secondSolve(b, target, N);
-    if (x < 0 && y < 0) cout << "-1 \n";
-    else if (x < 0 || y < 0) cout << max(x,y) << '\n';
-    else cout << min(x,y) << '\n';
+    string s;
+    cin >> s;
+    for (int i = 0; i < N; i++) A[i] = B[i] = s[i] - '0';
+    cin >> s;
+    for (int i = 0; i < N; i++) C[i] = s[i] - '0';
+}
+
+void change(const int & idx, int arr[]) {
+    for (int i = idx - 1; i < idx + 2; i++) {
+        if (i < 0 || i >= N) continue;
+        arr[i] = 1 - arr[i];
+    }
+}
+
+bool check(const int arr[]) {
+    for (int i = 0; i < N; i++) {
+        if (arr[i] != C[i]) return false;
+    }
+    return true;
+}
+
+int firstCheck() {
+    int ret = 1;
+    change(0, A);
+    for (int i = 1; i < N; i++) {
+        if (A[i - 1] != C[i - 1]) {
+            change(i, A);
+            ret++;
+        }
+    }
+    return check(A) ? ret : -1;
+}
+
+int secondCheck() {
+    int ret = 0;
+    for (int i = 1; i < N; i++) {
+        if (B[i - 1] != C[i - 1]) {
+            change(i, B);
+            ret++;
+        }
+    }
+    return check(B) ? ret : -1;
+}
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    get_input();
+    int x = firstCheck();
+    int y = secondCheck();
+    if (x >= 0 && y >= 0) cout << min(x, y) << '\n';
+    else if (x < 0 && y < 0) cout << -1 << '\n';
+    else cout << max(x, y) << '\n';
     return 0;
 }
