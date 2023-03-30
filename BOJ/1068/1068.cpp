1302b76d5a41a9f40e3d13e842c3ad6dff73f423
empodi <kglein9513@gmail.com>
2023-03-30 21:36:18 +0900

    ♻️  트리 - 골드 5 - 그래프

--- BOJ/1068/1068.cpp
+++ BOJ/1068/1068.cpp
@@ -1,48 +1,52 @@
-#include <cstdlib>
 #include <iostream>
 #include <vector>
+#define LEN 50
 using namespace std;
 
-int N, root, target;
-int answer = 0;
-vector<int> graph[55];
+int N, K, root, ret;
+vector<int> graph[LEN + 1];
+int parent[LEN + 1];
 
-void countLeaf(int cur)
-{
-    if (cur == target) return;
-    if (graph[cur].empty())
-    {
-        answer++;
-        return;
+void init() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        parent[i] = x;
+        if (x == -1) root = i;
+        else graph[x].push_back(i);
     }
-    int cnt = 0;
-    for (auto elem : graph[cur]) 
-    {
-        if (elem != target)
-        {
-            countLeaf(elem);
-            cnt++;
-        }
-    }
-    if (cnt == 0) answer++;
+    cin >> K;
+    ret = 0;
 }
 
-int main()
-{
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        int n;
-        cin >> n;
-        if (n == -1) root = i;
-        else graph[n].push_back(i); // n is parent
+void removeNode() {
+    int rParent = parent[K];
+    for (int i = 0; i < graph[rParent].size(); i++) {
+        if (graph[rParent][i] == K) {
+            graph[rParent][i] = -1;
+            break;
+        }
     }
-    
-    cin >> target;
-    graph[target].clear();
-    
-    countLeaf(root);
-    cout << answer << '\n';
+}
+
+void countLeaf(const int & pos) {
+    int cnt = 0;
+    for (auto e : graph[pos]) {
+        if (e < 0) continue;
+        countLeaf(e);
+        cnt++;
+    }
+    if (cnt == 0) ret++;
+}
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    init();
+    if (K != root) {
+        removeNode();
+        countLeaf(root);
+    }
+    cout << ret << '\n';
     return 0;
 }
