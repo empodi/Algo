e70f937e45306d20df3679630ee6cff62f48727e
empodi <kglein9513@gmail.com>
2023-04-01 17:55:10 +0900

    ♻️ ❗신을 모시는 사당 - 골드 5 - DP, 누적합

--- BOJ/27210/27210.cpp
+++ BOJ/27210/27210.cpp
@@ -1,30 +1,29 @@
-#include <cstdlib>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
+#define LEN 100000
 using namespace std;
 
-int main()
-{
+int N, pos, neg;
+int A[LEN + 1], B[LEN + 1], C[LEN + 1];
+
+int main() {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int pos = 0, neg = 0, N;
-    int A[100001] = {0,};
-    int B[100001] = {0,};
-    
+    memset(A, 0, sizeof(A));
+    memset(B, 0, sizeof(B));
+    memset(C, 0, sizeof(C));
     cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        int x;
-        cin >> x;
-        if (x == 2) x = -1;
-        if (i == 0) A[0] = B[0] = pos = neg = x;
-        else
-        {
-            A[i] = max(A[i - 1] + x, x);
-            B[i] = min(B[i - 1] + x, x);
-            pos = max(pos, A[i]);
-            neg = min(neg, B[i]);
-        }
+    for (int i = 1 ; i <= N; i++) {
+        cin >> A[i];
+        if (A[i] == 2) A[i] = -1;
     }
-    cout << max( abs(pos), abs(neg) ) << '\n';
+    pos = neg = 0;
+    for (int i = 1; i <= N; i++) {
+        B[i] = max(A[i], B[i - 1] + A[i]);
+        C[i] = min(A[i], C[i - 1] + A[i]);
+        pos = max(pos, B[i]);
+        neg = min(neg, C[i]);
+    }
+    cout << max(abs(pos), abs(neg)) << '\n';
     return 0;
 }
