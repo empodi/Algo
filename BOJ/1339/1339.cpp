2c2568a3d1417d0b6762ef0831d30222b0799d42
empodi <kglein9513@gmail.com>
2023-04-02 18:21:22 +0900

    ♻️  단어 수학 - 골드 4 - 그리디

--- BOJ/1339/1339.cpp
+++ BOJ/1339/1339.cpp
@@ -2,37 +2,67 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
-#include <math.h>
+#include <cmath>
 using namespace std;
-typedef long long ll;
 
-ll ret = 0;
 int N;
-vector<string> strList;
-unordered_map<char, ll> um;
-unordered_map<char, int> numHash;
+vector<string> v;
+unordered_map<char, long long> um;
 
-int main() {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+struct info {
+    char key;
+    long long priority;
+    int value;
+};
+
+vector<info> A;
+
+bool cmp(info a, info b) {
+    return a.priority > b.priority;
+}
+
+void init() {
     cin >> N;
-    for (int k = 0; k < N; k++) {
-        string s;
-        cin >> s;
-        strList.push_back(s);
+    v.resize(N);
+    for (int i = 0; i < N; i++) cin >> v[i];
+}
+
+void setPriority() {
+    for (auto & s : v) {
         for (int i = 0; i < s.length(); i++) {
             um[s[i]] += pow(10, s.length() - i);
         }
     }
-    vector<pair<ll,char>> v;
-    for (const auto & [key, value] : um) v.push_back({value,key});
-    sort(v.begin(), v.end());
-    for (int i = 0; i < v.size(); i++) {
-        numHash[v[i].second] = 10 - v.size() + i;
+    for (auto & [key, value] : um) {
+        A.push_back({key, value, 0});
     }
-    for (auto & s : strList) {
-        int cur = 0;
-        for (auto & elem : s) cur = cur * 10 + numHash[elem];
-        ret += cur;
+    sort(A.begin(), A.end(), cmp);
+    int p = 9;
+    for (auto & e : A) e.value = p--;
+//    for (auto & e : A) {
+//        cout << "key = " << e.key << "   value = " << e.value << "   priority = " << e.priority << '\n';
+//    }
+}
+
+int find(char key) {
+    for (auto & e : A) {
+        if (e.key == key) return e.value;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    init();
+    setPriority();
+
+    // calculate
+    int ret = 0;
+    for (auto & s : v) {
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            num = num * 10 + find(s[i]);
+        }
+        ret += num;
     }
     cout << ret << '\n';
     return 0;
